be4bc1be2f0f9051f1d43016a41fb0437e8049ec
Кушніренко Максим <100068913+Keernex@users.noreply.github.com>
2023-03-26 07:11:39 +0000

    done Ex-3

---
+++ main.py
@@ -0,0 +1,10 @@
+q = int(input())
+w = int(input())
+if q <= w:
+  for i in range(q, w + 1):
+    if i % 2 == 0:
+      print(i, end="  ")
+else:
+  for i in range(q, w - 1, -1):
+    if i % 2 == 0:
+      print(i, end="  ")
